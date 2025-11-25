625c23556a6fcb83a2235316d1a0c6d2f024e73c
char3176 <char3176@gmail.com>
2025-11-24 22:33:41 -0500

    add necessary extras to build.gradle

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package com.team3176.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Code_2025";
+  public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 265;
-  public static final String GIT_SHA = "05c5f52cef4a56af80bc0cfddad956ab3b83add6";
-  public static final String GIT_DATE = "2025-04-04 22:01:39 EDT";
-  public static final String GIT_BRANCH = "akit-run3";
-  public static final String BUILD_DATE = "2025-11-18 22:19:43 EST";
-  public static final long BUILD_UNIX_TIME = 1763522383539L;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "3b4e0d9c510b12919e1d91daa6fe4f79b986c083";
+  public static final String GIT_DATE = "2025-11-24 22:23:13 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-11-24 22:32:25 EST";
+  public static final long BUILD_UNIX_TIME = 1764041545773L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
